5fdec9c800963fe6afcde15afb08f3d6899da5f2
Jcaballerof22 <117446442+Jcaballerof22@users.noreply.github.com>
2023-04-20 11:40:38 +0200

    tusmuertos

--- src/loginBezeroGUI.java
+++ src/loginBezeroGUI.java
@@ -49,21 +49,21 @@ public class loginBezeroGUI {
 		frame.getContentPane().setLayout(null);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(180, 227, 118, 27);
+		passwordField.setBounds(180, 190, 118, 27);
 		frame.getContentPane().add(passwordField);
 		
 		textField = new JTextField();
-		textField.setBounds(180, 189, 118, 27);
+		textField.setBounds(180, 152, 118, 27);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Erabiltzailea");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblNewLabel.setBounds(84, 189, 66, 27);
+		lblNewLabel.setBounds(84, 152, 66, 27);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Pasahitza");
-		lblNewLabel_1.setBounds(84, 233, 46, 14);
+		lblNewLabel_1.setBounds(84, 196, 66, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("ONGI ETORRI");
@@ -72,19 +72,19 @@ public class loginBezeroGUI {
 		frame.getContentPane().add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Ez duzu konturik? Sortu bat.");
-		lblNewLabel_3.setBounds(123, 314, 135, 27);
+		lblNewLabel_3.setBounds(122, 285, 163, 27);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		JButton btnNewButton = new JButton("SARTU");
-		btnNewButton.setBounds(145, 280, 89, 23);
+		btnNewButton.setBounds(143, 243, 89, 23);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("ERREGISTRATU");
-		btnNewButton_1.setBounds(133, 353, 113, 23);
+		btnNewButton_1.setBounds(132, 330, 113, 23);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Saltzailea zara?");
-		btnNewButton_2.setBounds(133, 387, 113, 23);
+		btnNewButton_2.setBounds(132, 376, 113, 23);
 		frame.getContentPane().add(btnNewButton_2);
 	}
 }
