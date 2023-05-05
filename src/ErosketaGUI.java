76d2deb509d5749e4839d917496bfca2f9334035
afernandezi22 <117446404+afernandezi22@users.noreply.github.com>
2023-05-05 13:37:57 +0200

    Falta zen bat

--- src/ErosketaGUI.java
+++ src/ErosketaGUI.java
@@ -1,12 +1,4 @@
-
-/**
- * Erosketaren pantailaren GUI
- * @author Talde3
- * @version 05/05/2023
- */
-
 import java.awt.Font;
-
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -26,7 +18,11 @@ import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
-
+/**
+ * Erosketaren pantailaren GUI
+ * @author Talde3
+ * @version 05/05/2023
+ */
 public class ErosketaGUI extends JFrame {
 
 	private JPanel contentPane;
